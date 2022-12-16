cdb5326d25d5bc239b3ebeb057b46bce2efaf6b6
idrees <muhammad.idrees@gecurrent.com>
2022-12-16 14:23:21 +1100

    STEP-01 using spyOn to test print method of loggerService

--- src/app/services/calculator.service.spec.ts
+++ src/app/services/calculator.service.spec.ts
@@ -4,17 +4,29 @@ import { LoggerService } from "./logger.service";
 fdescribe("CalculatorService", () => {
   // Testing add function on calculator service
   it("should add two numbers", () => {
-    const calculator = new CalculatorService(new LoggerService());
+    const loggerService = new LoggerService();
+    // sypying on print method of logger service
+    spyOn(loggerService, "print");
 
+    const calculator = new CalculatorService(loggerService);
     const result = calculator.add(2, 2);
+
     expect(result).toBe(4);
+    // check print method is being called only once
+    expect(loggerService.print).toHaveBeenCalledTimes(1);
   });
 
   // Testing substract function on calculator service
   it("should substrat two numbers", () => {
-    const calculator = new CalculatorService(new LoggerService());
+    const loggerService = new LoggerService();
+    // sypying on print method of logger service
+    spyOn(loggerService, "print");
 
+    const calculator = new CalculatorService(new LoggerService());
     const result = calculator.substract(2, 2);
+
     expect(result).toBe(0);
+    // check print method is being called only once
+    expect(loggerService.print).toHaveBeenCalledTimes(1);
   });
 });
