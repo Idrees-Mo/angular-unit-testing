cf101c51c6b2bb9144933998aa686533e22508b1
idrees <muhammad.idrees@gecurrent.com>
2022-12-16 14:11:39 +1100

    STEP-01 very simple with clean code

--- src/app/services/calculator.service.spec.ts
+++ src/app/services/calculator.service.spec.ts
@@ -1,35 +1,20 @@
-// import { TestBed } from '@angular/core/testing';
-
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
 
 fdescribe("CalculatorService", () => {
-  let service: CalculatorService;
-
-  // ******** commented out the default beforEach **********
-  // beforeEach(() => {
-  //   TestBed.configureTestingModule({});
-  //   service = TestBed.inject(CalculatorService);
-  // });
-
-  // it("should be created", () => {
-  //   expect(service).toBeTruthy();
-  // });
-  //  ************************************************************
-
+  // Testing add function on calculator service
   it("should add two numbers", () => {
     const calculator = new CalculatorService(new LoggerService());
 
     const result = calculator.add(2, 2);
-
     expect(result).toBe(4);
   });
 
+  // Testing substract function on calculator service
   it("should substrat two numbers", () => {
     const calculator = new CalculatorService(new LoggerService());
 
     const result = calculator.substract(2, 2);
-
     expect(result).toBe(0);
   });
 });
