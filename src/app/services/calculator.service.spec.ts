48c8cc39d8486e22d362fb39382431f4f97fedab
idrees <muhammad.idrees@gecurrent.com>
2022-12-16 14:06:46 +1100

    Step-01 very basic test for add & subs number func

--- src/app/services/calculator.service.spec.ts
+++ src/app/services/calculator.service.spec.ts
@@ -1,16 +1,35 @@
-import { TestBed } from '@angular/core/testing';
+// import { TestBed } from '@angular/core/testing';
 
-import { CalculatorService } from './calculator.service';
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
 
-describe('CalculatorService', () => {
+fdescribe("CalculatorService", () => {
   let service: CalculatorService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CalculatorService);
+  // ******** commented out the default beforEach **********
+  // beforeEach(() => {
+  //   TestBed.configureTestingModule({});
+  //   service = TestBed.inject(CalculatorService);
+  // });
+
+  // it("should be created", () => {
+  //   expect(service).toBeTruthy();
+  // });
+  //  ************************************************************
+
+  it("should add two numbers", () => {
+    const calculator = new CalculatorService(new LoggerService());
+
+    const result = calculator.add(2, 2);
+
+    expect(result).toBe(4);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it("should substrat two numbers", () => {
+    const calculator = new CalculatorService(new LoggerService());
+
+    const result = calculator.substract(2, 2);
+
+    expect(result).toBe(0);
   });
 });
